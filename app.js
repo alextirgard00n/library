6490ebdcb09683634a9496dff983cd569f3db0ce
alextirgard00n <tirgardoon@gmail.com>
2023-04-18 21:29:26 -1000

    toggle modal implemented

--- app.js
+++ app.js
@@ -34,4 +34,41 @@ const el = document.createElement('div');
 el.classList.add('book-card');
 
 const bookCard = document.querySelector('.books-grid');
-// bookCard.appendChild(el);
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const addBookBtn = document.querySelector('.addBookBtn');
+const main = document.querySelector('main');
+const submitbtn = document.querySelector('.submit');
+
+// addBookBtn.onclick = () => addBook();
+
+// function addBook() {
+//     modal.classList.add('active');
+//     overlay.classList.add('active');
+
+// }
+
+addBookBtn.onclick = () => {
+    toggleModal();
+};
+
+// addBookBtn.onclick = toggleModal;
+
+
+// submitbtn.onclick = () => {
+//     modal.classList.toggle('active');
+//     overlay.classList.toggle('active');
+// }
+
+function toggleModal() {
+    modal.classList.toggle('active');
+    overlay.classList.toggle('active');
+}
+//removes form clicking anywhere on overlay
+overlay.addEventListener('click', function () {
+    if (modal.classList.contains('active')) {
+        toggleModal();
+    }
+});
+
